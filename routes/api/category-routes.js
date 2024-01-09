863c8da421c4170b7ea98bd5d64762fb5574272b
tjamesmcdonald <tjames.mcdonald@gmail.com>
2024-01-09 16:47:49 -0500

    repo init

---
+++ routes/api/category-routes.js
@@ -0,0 +1,77 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  try {
+    const categoriesData = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(categoriesData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new category
+  try {
+    const categoryData = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      },
+      category_name: req.body.category_name
+    })
+    res.status(200).json({ message: 'Category Name Updated!'})
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      }
+    })
+    if (!categoryData) {
+      res.status(404).json({ message: 'No Category found with matching id!' });
+      return
+    }
+    res.status(200).json({ message: 'Category successfully Deleted!'})
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+module.exports = router;
